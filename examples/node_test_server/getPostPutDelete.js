829cb2d1f7129a69cc18e13a2dc75ea51f841bb2
Sandeep Mistry <s.mistry@arduino.cc>
2016-06-28 16:49:15 -0400

    Make whitespace consistent in getPostPutDelete.js

--- examples/node_test_server/getPostPutDelete.js
+++ examples/node_test_server/getPostPutDelete.js
@@ -8,7 +8,7 @@
 */
 
 var express = require('express');			// include express.js
-var app = express();						      // a local instance of it
+var app = express();						// a local instance of it
 var bodyParser = require('body-parser');	// include body-parser
 
 // you need a  body parser:
@@ -16,8 +16,8 @@ app.use(bodyParser.urlencoded({extended: false})); // for application/x-www-form
 
 // this runs after the server successfully starts:
 function serverStart() {
-  var port = server.address().port;
-  console.log('Server listening on port '+ port);
+	var port = server.address().port;
+	console.log('Server listening on port '+ port);
 }
 
 // this is the POST handler:
@@ -26,12 +26,12 @@ app.all('/*', function (request, response) {
 	// the parameters of a GET request are passed in
 	// request.body. Pass that to formatResponse()
 	// for formatting:
-  console.log(request.headers);
-  if (request.method == 'GET') {
-    console.log(request.query);
-  } else {
-    console.log(request.body);
-  }
+	console.log(request.headers);
+	if (request.method == 'GET') {
+		console.log(request.query);
+	} else {
+		console.log(request.body);
+	}
 
 	// send the response:
 	response.send('OK');
