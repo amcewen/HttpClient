bdb8c02702b4ffe7534e60e0095c217affe7058e
Sandeep Mistry <s.mistry@arduino.cc>
2017-01-03 16:33:14 -0500

    Add new basic auth example

---
+++ examples/BasicAuthGet/BasicAuthGet.ino
@@ -0,0 +1,69 @@
+/*
+  GET client with HTTP basic authentication for ArduinoHttpClient library
+  Connects to server once every five seconds, sends a GET request
+
+  note: WiFi SSID and password are stored in config.h file.
+  If it is not present, add a new tab, call it "config.h" 
+  and add the following variables:
+  char ssid[] = "ssid";     //  your network SSID (name)
+  char pass[] = "password"; // your network password
+
+  created 14 Feb 2016
+  by Tom Igoe
+  modified 3 Jan 2017 to add HTTP basic authentication
+  by Sandeep Mistry
+  
+  this example is in the public domain
+ */
+#include <ArduinoHttpClient.h>
+#include <WiFi101.h>
+#include "config.h"
+
+char serverAddress[] = "192.168.0.3";  // server address
+int port = 8080;
+
+WiFiClient wifi;
+HttpClient client = HttpClient(wifi, serverAddress, port);
+int status = WL_IDLE_STATUS;
+String response;
+int statusCode = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to Network named: ");
+    Serial.println(ssid);                   // print the network name (SSID);
+
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+  }
+
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+}
+
+void loop() {
+  Serial.println("making GET request with HTTP basic authentication");
+  client.beginRequest();
+  client.get("/secure");
+  client.sendBasicAuth("username", "password"); // send the username and password for authentication
+  client.endRequest();
+
+  // read the status code and body of the response
+  statusCode = client.responseStatusCode();
+  response = client.responseBody();
+
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.print("Response: ");
+  Serial.println(response);
+  Serial.println("Wait five seconds");
+  delay(5000);
+}
+
